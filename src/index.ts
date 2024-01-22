9432770904aea070b3a82f204b1c8fea3738aca3
diligite-iustitiam <107812456+diligite-iustitiam@users.noreply.github.com>
2024-01-22 21:21:35 +0200

    fixed conflicts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
-import express from 'express';
-require('dotenv').config();
+import { Container, ContainerModule, interfaces } from 'inversify';
+import { ILogger } from './logger/logger.interface';
+import { App } from './app';
+import { ExceptionFilter } from './errors/exception.filter';
+import { LoggerService } from './logger/logger.service';
+import { UserController } from './users/users.controller';
+import { TYPES } from './types/types';
+import { IExceptionFilter } from './errors/exception.filter.interface';
+import { ProductController } from './products/products.controller';
 
-const port = 3000;
-const app = express();
+export interface IBootstrapReturn {
+  appContainer: Container;
+  app: App;
+}
 
-app.get('/', (req, res) => {
-  res.send('Welcome to gadjets store');
+export const appBindings = new ContainerModule((bind: interfaces.Bind) => {
+  bind<ILogger>(TYPES.ILogger).to(LoggerService);
+  bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+  bind<UserController>(TYPES.UserController).to(UserController);
+  bind<ProductController>(TYPES.ProductController).to(ProductController);
+  bind<App>(TYPES.Application).to(App);
 });
 
-app.listen(port, () => {
-  console.log(`Server started on http://localhost:${port}`);
-});
+function bootstrap(): IBootstrapReturn {
+  const appContainer = new Container();
+  appContainer.load(appBindings);
+  const app = appContainer.get<App>(TYPES.Application);
+  app.init();
+  return { app, appContainer };
+}
+
+export const { app, appContainer } = bootstrap();
